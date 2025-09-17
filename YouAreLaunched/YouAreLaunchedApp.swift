59362a35167965aaf2c2fbba2b01481cdba4a93b
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2025-09-17 16:53:21 +0300

    Initial Commit

---
+++ YouAreLaunched/YouAreLaunchedApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreLaunchedApp.swift
+//  YouAreLaunched
+//
+//  Created by Michael on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreLaunchedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
